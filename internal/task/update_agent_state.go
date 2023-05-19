87144ed6f18c8da2153e1178325121205afc1aa5
Sydonian <794346190@qq.com>
2023-05-19 15:19:50 +0800

    增加更新节点状态的任务

---
+++ internal/task/update_agent_state.go
@@ -0,0 +1,34 @@
+package task
+
+import (
+	"gitlink.org.cn/cloudream/common/consts"
+	mysql "gitlink.org.cn/cloudream/db/sql"
+	"gitlink.org.cn/cloudream/utils/logger"
+)
+
+type UpdateAgentStateTask struct {
+	NodeID     int
+	IPFSStatus string
+}
+
+func (t *UpdateAgentStateTask) TryMerge(other Task) bool {
+	return false
+}
+
+func (t *UpdateAgentStateTask) Execute(execCtx *ExecuteContext, execOpts ExecuteOption) {
+	if t.IPFSStatus != consts.IPFS_STATUS_OK {
+		logger.WithField("NodeID", t.NodeID).Warnf("IPFS status is %s, set node state unavailable", t.IPFSStatus)
+
+		err := mysql.Node.ChangeState(execCtx.DB.SQLCtx(), t.NodeID, consts.NODE_STATE_UNAVAILABLE)
+		if err != nil {
+			logger.WithField("NodeID", t.NodeID).Warnf("change node state failed, err: %s", err.Error())
+		}
+		return
+	}
+
+	// TODO 如果以后还有其他的状态，要判断哪些状态下能设置Normal
+	err := mysql.Node.ChangeState(execCtx.DB.SQLCtx(), t.NodeID, consts.NODE_STATE_NORMAL)
+	if err != nil {
+		logger.WithField("NodeID", t.NodeID).Warnf("change node state failed, err: %s", err.Error())
+	}
+}
