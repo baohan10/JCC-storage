eb5991670197cb30efaa5b9ac2cd5f605fbf077d
Sydonian <794346190@qq.com>
2023-08-01 10:50:17 +0800

    优化编译脚本

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -4,19 +4,17 @@ package main
 
 import (
 	"magefiles"
-	"magefiles/utils"
 
 	//mage:import
-	"magefiles/targets"
+	_ "magefiles/targets"
 )
 
 var Default = Build
 
 func Build() error {
-	return utils.Build(magefiles.BuildArgs{
-		OutputBinName: "coordinator",
-		OutputDirPath: "coordinator",
-		AssetsDir:     "assets",
-		PubArgs:       targets.PubGoBuildArgs,
+	return magefiles.Build(magefiles.BuildArgs{
+		OutputName: "coordinator",
+		OutputDir:  "../../build/coordinator",
+		AssetsDir:  "assets",
 	})
 }
